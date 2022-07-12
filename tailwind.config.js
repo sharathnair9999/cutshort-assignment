586d264e636982c2efcafcf3dbda7526ca1ca441
Sharath <sharathnair9999@gmail.com>
2022-07-13 00:19:31 +0530

    initial setup and cleanup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cta: "#664de5",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
