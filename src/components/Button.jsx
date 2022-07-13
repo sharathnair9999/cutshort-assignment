6321398c655c099ead7d91b067e6d03a646ff17a
Sharath <sharathnair9999@gmail.com>
2022-07-13 17:06:37 +0530

    button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Button = () => {
-  return <button>Button</button>;
+const Button = ({ btnText = "Create Workspace", clickHandler, className }) => {
+  return (
+    <button
+      type="submit"
+      onClick={clickHandler}
+      className={`w-full max-w-sm py-3 text-white text-sm rounded-md bg-cta hover:bg-cta/95 transition-all hover:shadow-md ${className}`}
+    >
+      {btnText}
+    </button>
+  );
 };
 
 export default Button;
